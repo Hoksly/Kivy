2ad82361ba0a3cba6ba26388d632f3f2ac651679
Andrew <realhoksly@gmail.com>
2020-11-06 09:59:26 +0200

    V.1.0.4

--- main.py
+++ main.py
@@ -19,10 +19,9 @@ from kivy.uix.scrollview import ScrollView
 
 import os
 
-
 # Globals:
-FILE_IMAGES = {'.txt' : 'txt.png', '.mp3': 'mp3.png', '.log': 'txt.png'}
-W_HEIGHT = 50  # height of folder and file widgets
+FILE_IMAGES = {'.txt': 'txt.png', '.mp3': 'mp3.png', '.log': 'txt.png'}
+W_HEIGHT = Window.height // 10  # height of folder and file widgets
 P_W_DISTANCE = 0  # distance between PageWidget and Folder/File Widget
 Manager = ScreenManager()
 
@@ -35,38 +34,54 @@ def if_dir(path):
         return False
 
 
+def give_previous_dir(name):
+    if name == '/':
+        return '/'
+    else:
+        return name[:name.rindex('/')]
+
+
 def show_dirs_and_files(directory, show_secrets=False):
     files = []
     dirs = []
+    try:
 
-    if show_secrets:
-        for el in os.listdir(directory):
-            if if_dir(directory + '/' + el):
-                dirs.append(el)
-            else:
-                files.append(el)
-
-    else:
-        for el in os.listdir(directory):
-            if el[0] != '.':
+        if show_secrets:
+            for el in os.listdir(directory):
                 if if_dir(directory + '/' + el):
                     dirs.append(el)
                 else:
                     files.append(el)
 
+        else:
+            for el in os.listdir(directory):
+                if el[0] != '.':
+                    if if_dir(directory + '/' + el):
+                        dirs.append(el)
+                    else:
+                        files.append(el)
+    except:
+        return  [], []
+
     return sorted(files), sorted(dirs)
 
 
-def build_page(files, dirs, dir_name = ''):
+def build_page(files, dirs, dir_name=''):
     # Idk how, but it works
     root = GridLayout(cols=1, size_hint_y=None)
     root.bind(minimum_height=root.setter('height'))
 
     if root.height < W_HEIGHT * (len(dirs) + len(files)):
-        # Config.set('graphics', 'height',  str(W_HEIGHT * (len(dirs) + len(files))))
         root.size = (Window.width, W_HEIGHT * (len(dirs) + len(files)))
 
     top = root.height
+
+    f = GoBackFolderWidget(pos=(P_W_DISTANCE, top - W_HEIGHT), height=W_HEIGHT, width=root.width)
+    root.add_widget(f)
+    top -= W_HEIGHT
+    f.full_name = give_previous_dir(dir_name)
+    f.main_dir = dir_name
+    f.ids.folder_label.text = '...'
 
     for el in dirs:
         i = dirs.index(el) + 1
@@ -76,13 +91,14 @@ def build_page(files, dirs, dir_name = ''):
         f.full_name = dir_name + '/' + el
 
         root.add_widget(f)
+        print(f.height)
         del f
 
     top -= len(dirs) * W_HEIGHT
 
     for el in files:
         i = files.index(el) + 1
-        f = FileWidget(pos = (P_W_DISTANCE,top- i * W_HEIGHT), height = W_HEIGHT, width = root.width)
+        f = FileWidget(pos=(P_W_DISTANCE, top - i * W_HEIGHT), height=W_HEIGHT, width=root.width)
         try:
             f.ids.file_label.text = el
         except ValueError:
@@ -103,7 +119,8 @@ def build_page(files, dirs, dir_name = ''):
         root.add_widget(f)
         del f, c_name, img_source
 
-    s = ScrollView(size = Window.size)
+
+    s = ScrollView(size=Window.size)
     s.add_widget(root)
 
     screen = Screen()
@@ -119,7 +136,7 @@ class PageApp(App):
 
     def build(self):
         f, d = show_dirs_and_files(self.folder)
-        page = build_page(f, d, dir_name= self.folder)
+        page = build_page(f, d, dir_name=self.folder)
         Manager.add_widget(page)
         del f, d, page
         return Manager
@@ -135,6 +152,8 @@ class FolderWidget(Widget):
         page = build_page(f, d, self.full_name)
         Manager.add_widget(page)
         Manager.remove_widget(Manager.get_screen(self.full_name[:self.full_name.rindex('/')]))
+        Manager.transition.direction = 'left'
+        Manager.current = self.full_name
         del f, d, page
 
 
@@ -143,7 +162,24 @@ class FileWidget(Widget):
     file_name = "<Name_error>"
 
 
+class GoBackFolderWidget(FolderWidget):
+    full_name = '/'
+    main_dir = '/'
+
+    def switch_to(self):
+        global Manager
+        try:
+            f, d = show_dirs_and_files(self.full_name)
+            page = build_page(f, d, self.full_name)
+            Manager.add_widget(page)
+            Manager.transition.direction = 'right'
+            Manager.current = self.full_name
+            Manager.remove_widget(Manager.get_screen(self.main_dir))
+            del f, d, page
+        except:
+            pass
+
 if __name__ == '__main__':
     main = PageApp()
-    main.folder = '/home'
+    main.folder = '/home/hoksly/Github'
     main.run()
